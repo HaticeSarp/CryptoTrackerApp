db5d5d5451c9f7d766d77b72019ef85edc1f9d40
Hatice Sarp <h.sarp2017@gmail.com>
2026-02-18 15:17:20 +0300

    feat(model): add coin model with codasble support

---
+++ New Group/CryptoTrackerApp/CryptoTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerAppApp.swift
+//  CryptoTrackerApp
+//
+//  Created by Hatice Sarp on 18.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
