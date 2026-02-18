8027e5bfbc1f78bc7b684e37616e4f9d535f9cf6
Hatice Sarp <h.sarp2017@gmail.com>
2026-02-18 15:24:29 +0300

    feat(service): implement CoinService with async/await

---
+++ CryptoTrackerApp/Models/Coin.swift
@@ -0,0 +1,25 @@
+//
+//  Coin.swift
+//  CryptoTrackerApp
+//
+//  Created by Hatice Sarp on 18.02.2026.
+//
+import Foundation
+
+struct Coin: Identifiable, Codable {
+    let id: String
+    let name: String
+    let symbol: String
+    let image: String
+    let currentPrice: Double
+    let priceChangePercentage24H: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case symbol
+        case image
+        case currentPrice = "current_price"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+    }
+}
---
+++ CryptoTrackerApp/Services/CoinService.swift
@@ -0,0 +1,39 @@
+//
+//  CoinService.swift
+//  CryptoTrackerApp
+//
+//  Created by Hatice Sarp on 18.02.2026.
+//
+import Foundation
+
+enum NetworkError: Error {
+    case invalidUrl
+    case invalidResponse
+    case decodingError
+}
+
+struct CoinService {
+    private let baseUrl: String = "https://api.coingecko.com/api/v3"
+}
+
+    
+    func fetchCoins() async throws -> [Coin] {
+        guard let url = URL(string: """
+            https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1
+            """) else {
+                throw NetworkError.invalidURL
+            }
+        
+        let (data, response) =  try await URLSession.shared.data(from: <#T##URL#>)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+                    httpResponse.statusCode == 200 else {
+                  throw NetworkError.invalidResponse
+              }
+        
+        do {
+                  return try JSONDecoder().decode([Coin].self, from: data)
+              } catch {
+                  throw NetworkError.decodingError
+              }
+    }
